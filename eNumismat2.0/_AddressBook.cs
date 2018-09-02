7db819f26bee9af1c7133b336aa680ab55233781
Christoph Caina <admin@caina.de>
2018-09-02 20:44:01 +0200

    simplified if else for contactcounter toolstrip text

--- eNumismat2.0/_AddressBook.cs
+++ eNumismat2.0/_AddressBook.cs
@@ -11,21 +11,25 @@ using DevComponents.DotNetBar;
 
 namespace eNumismat2._0
 {
+    //=====================================================================================================================================================================
     public partial class _AddressBook : RibbonForm
     {
         Classes.DataBaseWork DBWorker;
 
+        //=====================================================================================================================================================================
         public _AddressBook()
         {
             InitializeComponent();
         }
 
+        //=====================================================================================================================================================================
         private void _AddressBook_Load(object sender, EventArgs e)
         {
             DBWorker = new Classes.DataBaseWork();
             GetContactsCount();
         }
 
+        //=====================================================================================================================================================================
         private void GetContactsCount()
         {
             int ContactCounter = 0;
@@ -39,7 +43,7 @@ namespace eNumismat2._0
                 MessageBox.Show(ex.Message);
             }
 
-            if (ContactCounter == 0)
+            if (ContactCounter == 0 || ContactCounter > 1)
             {
                 toolStripStatusLabel1.Text = ContactCounter.ToString() + " " + GlobalStrings._contactsAvailable;
 
@@ -47,11 +51,6 @@ namespace eNumismat2._0
             else if (ContactCounter == 1)
             {
                 toolStripStatusLabel1.Text = ContactCounter.ToString() + " " + GlobalStrings._contactAvailable;
-
-            }
-            else if (ContactCounter > 1)
-            {
-                toolStripStatusLabel1.Text = ContactCounter.ToString() + " " + GlobalStrings._contactsAvailable;
 
             }
         }
